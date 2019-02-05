6b504a3e60f1dae74e186dafe99ae2326e9e0d48
Eoin Campbell <campbeeo@tcd.ie>
2019-02-05 23:24:31 +0000

    Update version

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2019012202;
+$plugin->version  = 2019020500;
 $plugin->requires = 2018050800; // Requires 3.5.
-$plugin->release = '3.5.5c';
+$plugin->release = '3.5.5d';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->cron     = 0;
 $plugin->component = 'mod_attendance';
